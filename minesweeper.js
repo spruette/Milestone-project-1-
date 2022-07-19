d0b6645086190bf43d36141216b3b96b06d1feeb
unknown <scotteychaos@gmail.com>
2022-07-19 18:21:38 -0400

    board now has tiles

--- minesweeper.js
+++ minesweeper.js
@@ -16,4 +16,18 @@ window.onload = function () {
 
 function startGame () {
     document.getElementById("mines-count").innerText = minesCount;
+
+    //populate our board
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "." + c.toString();
+            document.getElementById("board").append(tile);
+            row.push(tile);
+
+        }
+        board.push(row);
+    }
+    console.log(board);
 }
